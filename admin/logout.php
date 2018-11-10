a42d7e8acd0f6badd95d3cf7e65fa87890c71479
Carl-Ivan <carl4christ.22@gmail.com>
2018-11-10 01:38:19 +0100

    Initial commit

---
+++ admin/logout.php
@@ -0,0 +1,11 @@
+<?php session_start(); ?>
+
+<?php
+  $_SESSION['username'] = null;
+  $_SESSION['userID'] = null;
+  $_SESSION['user_role'] = null;
+  $_SESSION['email'] = null;
+
+  header("Location: ../index.php");
+
+?>
